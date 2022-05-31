d9dfaea6052d64fbd70e74b170d0c6a8d21f6336
rafaelseifert <rs.frontend@gmail.com>
2022-05-30 21:44:03 -0600

    feat: adjust document title to brand name

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
+  title: "Tableton",
   viewport: "width=device-width,initial-scale=1"
 });
 
