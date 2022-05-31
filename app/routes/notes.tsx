3839327259d616b765e74c39012d62816eb689c9
rafaelseifert <rs.frontend@gmail.com>
2022-05-31 12:52:33 -0600

    ref: extract logout-button

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,10 +1,12 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Form, Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
+import { Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
 
 import { requireUserId } from "~/session.server";
 import { useUser } from "~/utils";
 import { getNoteListItems } from "~/models/note.server";
+import LogoutButton from "~/components/LogoutButton";
+import { ROUTES } from "~/constants";
 
 type LoaderData = {
   noteListItems: Awaited<ReturnType<typeof getNoteListItems>>;
@@ -24,17 +26,10 @@ export default function NotesPage() {
     <div className="flex h-full min-h-screen flex-col">
       <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
         <h1 className="text-3xl font-bold">
-          <Link to=".">Notes</Link>
+          <Link to={ROUTES.DASHBOARD}>Notes</Link>
         </h1>
         <p>{user.email}</p>
-        <Form action="/logout" method="post">
-          <button
-            type="submit"
-            className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
-          >
-            Logout
-          </button>
-        </Form>
+        <LogoutButton />
       </header>
 
       <main className="flex h-full bg-white">
