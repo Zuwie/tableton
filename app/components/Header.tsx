5bee06ef86a9529c3d26cc5cfc658a487d1f62f0
rafaelseifert <rs.frontend@gmail.com>
2022-05-31 14:25:17 -0600

    chore: cleanup

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,42 +1,6 @@
 import { Popover } from "@headlessui/react";
-import {
-  BookmarkAltIcon,
-  CalendarIcon,
-  ShieldCheckIcon,
-  SupportIcon,
-} from "@heroicons/react/outline";
 import NavDesktop from "~/components/NavDesktop";
 import NavMobile from "~/components/NavMobile";
-
-const resources = [
-  {
-    name: "Help Center",
-    description:
-      "Get all of your questions answered in our forums or contact support.",
-    href: "#",
-    icon: SupportIcon,
-  },
-  {
-    name: "Guides",
-    description:
-      "Learn how to maximize our platform to get the most out of it.",
-    href: "#",
-    icon: BookmarkAltIcon,
-  },
-  {
-    name: "Events",
-    description:
-      "See what meet-ups and other events we might be planning near you.",
-    href: "#",
-    icon: CalendarIcon,
-  },
-  {
-    name: "Security",
-    description: "Understand how we take your privacy seriously.",
-    href: "#",
-    icon: ShieldCheckIcon,
-  },
-];
 
 export default function Header() {
   return (
