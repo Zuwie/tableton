9a68ea3cf3612713d95d8c6b7170b52d64f5d73e
rafaelseifert <rs.frontend@gmail.com>
2022-06-01 14:15:41 -0600

    feat: install chakra-ui by following official guide

--- app/root.tsx
+++ app/root.tsx
@@ -14,16 +14,74 @@ import {
 } from "@remix-run/react";
 import { getUser } from "./session.server";
 import styles from "./styles/app.css";
-
-export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: styles }];
-};
+import { useContext, useEffect } from "react";
+import { ClientStyleContext, ServerStyleContext } from "~/context";
+import { ChakraProvider } from "@chakra-ui/provider";
+import { withEmotionCache } from "@emotion/react";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Tableton",
-  viewport: "width=device-width,initial-scale=",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
 });
+
+export let links: LinksFunction = () => {
+  return [
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    { rel: "preconnect", href: "https://fonts.gstatic.com" },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap",
+    },
+  ];
+};
+
+interface DocumentProps {
+  children: React.ReactNode;
+}
+
+const Document = withEmotionCache(
+  ({ children }: DocumentProps, emotionCache) => {
+    const serverStyleData = useContext(ServerStyleContext);
+    const clientStyleData = useContext(ClientStyleContext);
+
+    // Only executed on client
+    useEffect(() => {
+      // re-link sheet container
+      emotionCache.sheet.container = document.head;
+      // re-inject tags
+      const tags = emotionCache.sheet.tags;
+      emotionCache.sheet.flush();
+      tags.forEach((tag) => {
+        (emotionCache.sheet as any)._insertTag(tag);
+      });
+      // reset cache to reapply global styles
+      clientStyleData?.reset();
+    }, []);
+
+    return (
+      <html lang="en">
+        <head>
+          <Meta />
+          <Links />
+          {serverStyleData?.map(({ key, ids, css }) => (
+            <style
+              key={key}
+              data-emotion={`${key} ${ids.join(" ")}`}
+              dangerouslySetInnerHTML={{ __html: css }}
+            />
+          ))}
+        </head>
+        <body>
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </body>
+      </html>
+    );
+  }
+);
 
 type LoaderData = {
   user: Awaited<ReturnType<typeof getUser>>;
@@ -37,17 +95,10 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 export default function App() {
   return (
-    <html lang="en" className="h-full">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body className="h-full">
+    <Document>
+      <ChakraProvider>
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
+      </ChakraProvider>
+    </Document>
   );
 }
