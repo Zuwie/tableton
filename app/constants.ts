488f17907227b0581d79a3a7d6be8108e476b305
rafaelseifert <rs.frontend@gmail.com>
2022-06-01 17:19:33 -0600

    feat: add additional routes for later use

--- app/constants.ts
+++ app/constants.ts
@@ -2,6 +2,8 @@ export const ROUTES = {
   ROOT: "/",
   LOGIN: "/login",
   JOIN: "/join",
+  RESET_PASSWORD: "/reset-password",
   DASHBOARD: "/dashboard",
+  FEATURES: "/features",
   ACCOUNT: "/account",
 };
