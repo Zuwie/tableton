019552b56f824ef05f2515f9e3e828f9eaeba713
rafaelseifert <rs.frontend@gmail.com>
2022-06-02 17:38:28 -0600

    chore: remove any styling from logout-button to reuse it freely

--- app/components/LogoutButton.tsx
+++ app/components/LogoutButton.tsx
@@ -1,18 +1,9 @@
 import { Form } from "@remix-run/react";
-import { Button } from "@chakra-ui/react";
 
 export default function LogoutButton() {
   return (
     <Form action="/logout" method="post">
-      <Button
-        type={"submit"}
-        fontWeight={600}
-        color={"white"}
-        bg={"green.600"}
-        _hover={{ bg: "green.700" }}
-      >
-        Logout
-      </Button>
+      <button type="submit">Sign out</button>
     </Form>
   );
 }
