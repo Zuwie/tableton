a8e3057d208a32692682555eb9e9d35e97245189
rafaelseifert <rs.frontend@gmail.com>
2022-06-11 17:09:18 -0600

    fix: wrong color flash

--- app/chakra-theme.ts
+++ app/chakra-theme.ts
@@ -2,7 +2,7 @@ import type { ThemeConfig } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
-  initialColorMode: "system",
+  initialColorMode: "light",
   useSystemColorMode: false,
 };
 
