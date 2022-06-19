f77f0d56c2c1a28e22123fe2756d863b7468853b
rafaelseifert <rs.frontend@gmail.com>
2022-06-18 19:01:01 -0500

    feat: add waves

--- app/components/LandingFeatures.tsx
+++ app/components/LandingFeatures.tsx
@@ -1,90 +1,87 @@
 import type { ReactNode } from "react";
 import {
-  Stack,
-  Container,
   Box,
-  Flex,
-  Text,
+  Container,
   Heading,
   SimpleGrid,
+  Stack,
+  Text,
 } from "@chakra-ui/react";
 
 export default function LandingFeatures() {
   return (
-    <Box bg={"gray.800"} position={"relative"}>
-      <Flex
-        flex={1}
-        zIndex={0}
-        display={{ base: "none", lg: "flex" }}
-        backgroundImage="url('/templates/stats-grid-with-image.png')"
-        backgroundSize={"cover"}
-        backgroundPosition="center"
-        backgroundRepeat="no-repeat"
-        position={"absolute"}
-        insetY={0}
-        right={0}
-      >
-        <Flex
-          bgGradient={"linear(to-r, gray.800 10%, transparent)"}
-          w={"full"}
-          h={"full"}
-        />
-      </Flex>
-      <Container maxW={"6xl"} zIndex={10} position={"relative"} centerContent>
-        <Stack direction={{ base: "column", lg: "row" }}>
-          <Stack
-            flex={1}
-            color={"gray.400"}
-            justify={{ lg: "center" }}
-            py={{ base: 4, md: 20, xl: 60 }}
-          >
-            <Box mb={{ base: 8, md: 20 }}>
-              <Text
-                fontFamily={"heading"}
-                fontWeight={700}
-                textTransform={"uppercase"}
-                mb={3}
-                fontSize={"xl"}
-                color={"gray.500"}
-              >
-                Technology
-              </Text>
-              <Heading
-                color={"white"}
-                mb={5}
-                fontSize={{ base: "3xl", md: "5xl" }}
-              >
-                21st century agriculture
-              </Heading>
-              <Text fontSize={"xl"} color={"gray.400"}>
-                The NewLife™ technology allows you to monitor your crops and get
-                complete insights at real time. The proprietary
-                software/hardware ecosystem prevents your plants from getting
-                neglected.
-              </Text>
-            </Box>
+    <>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#1A202C"
+          fill-opacity="1"
+          d="M0,320L60,277.3C120,235,240,149,360,144C480,139,600,213,720,224C840,235,960,181,1080,165.3C1200,149,1320,171,1380,181.3L1440,192L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+        ></path>
+      </svg>
+      <Box bg={"gray.800"} position={"relative"}>
+        <Container maxW={"6xl"} zIndex={10} position={"relative"} centerContent>
+          <Stack direction={{ base: "column", lg: "row" }}>
+            <Stack
+              flex={1}
+              color={"gray.400"}
+              justify={{ lg: "center" }}
+              py={{ base: 4, md: 20 }}
+            >
+              <Box mb={{ base: 8, md: 20 }}>
+                <Text
+                  fontFamily={"heading"}
+                  fontWeight={700}
+                  textTransform={"uppercase"}
+                  mb={3}
+                  fontSize={"xl"}
+                  color={"gray.500"}
+                >
+                  Technology
+                </Text>
+                <Heading
+                  color={"white"}
+                  mb={5}
+                  fontSize={{ base: "3xl", md: "5xl" }}
+                >
+                  21st century agriculture
+                </Heading>
+                <Text fontSize={"xl"} color={"gray.400"}>
+                  The NewLife™ technology allows you to monitor your crops and
+                  get complete insights at real time. The proprietary
+                  software/hardware ecosystem prevents your plants from getting
+                  neglected.
+                </Text>
+              </Box>
 
-            <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
-              {stats.map((stat) => (
-                <Box key={stat.title}>
-                  <Text
-                    fontFamily={"heading"}
-                    fontSize={"3xl"}
-                    color={"white"}
-                    mb={3}
-                  >
-                    {stat.title}
-                  </Text>
-                  <Text fontSize={"xl"} color={"gray.400"}>
-                    {stat.content}
-                  </Text>
-                </Box>
-              ))}
-            </SimpleGrid>
+              <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+                {stats.map((stat) => (
+                  <Box key={stat.title}>
+                    <Text
+                      fontFamily={"heading"}
+                      fontSize={"3xl"}
+                      color={"white"}
+                      mb={3}
+                    >
+                      {stat.title}
+                    </Text>
+                    <Text fontSize={"xl"} color={"gray.400"}>
+                      {stat.content}
+                    </Text>
+                  </Box>
+                ))}
+              </SimpleGrid>
+            </Stack>
           </Stack>
-        </Stack>
-      </Container>
-    </Box>
+        </Container>
+      </Box>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#1A202C"
+          fill-opacity="1"
+          d="M0,192L48,208C96,224,192,256,288,234.7C384,213,480,139,576,106.7C672,75,768,85,864,106.7C960,128,1056,160,1152,181.3C1248,203,1344,213,1392,218.7L1440,224L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+        ></path>
+      </svg>
+    </>
   );
 }
 
