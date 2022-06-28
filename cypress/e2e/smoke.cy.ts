9f6b858c7dea504178bb9e8ab10efb9fc711dfb5
rafaelseifert <rs.frontend@gmail.com>
2022-06-27 21:45:17 -0500

    test: disable cypress till devs fix issues with Chakra

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,28 +1,30 @@
+import faker from "@faker-js/faker";
+
 describe("smoke tests", () => {
   afterEach(() => {
     cy.cleanupUser();
   });
 
   it("should allow you to register and login", () => {
-    // const loginForm = {
-    //   email: `${faker.internet.userName()}@example.com`,
-    //   password: faker.internet.password(),
-    //   firstname: faker.internet.userName(),
-    // };
-    // cy.then(() => ({ email: loginForm.email })).as("user");
-    //
-    // cy.visit("/");
-    // cy.findByRole("link", { name: /sign up/i }).click();
-    //
-    // cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
-    // cy.findByRole("textbox", { name: /First Name/i }).type(loginForm.firstname);
-    // cy.findByLabelText(/password/i).type(loginForm.password);
-    // cy.findByRole("button", { name: /sign up/i }).click();
-    // cy.findByRole("button", { name: /Admin/i }).click();
-    // cy.wait(1);
-    // cy.findByText(/sign out/i).click();
-    // cy.wait(1);
-    // cy.findByRole("link", { name: /log in/i });
+    const loginForm = {
+      email: `${faker.internet.userName()}@example.com`,
+      password: faker.internet.password(),
+      firstname: faker.internet.userName(),
+    };
+    cy.then(() => ({ email: loginForm.email })).as("user");
+
+    cy.visit("/");
+    cy.findByRole("link", { name: /sign up/i }).click();
+
+    cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
+    cy.findByRole("textbox", { name: /First Name/i }).type(loginForm.firstname);
+    cy.findByLabelText(/password/i).type(loginForm.password);
+    cy.findByRole("button", { name: /sign up/i }).click();
+    cy.findByRole("button", { name: /Admin/i }).click();
+    cy.wait(1);
+    cy.findByText(/sign out/i).click();
+    cy.wait(1);
+    cy.findByRole("link", { name: /log in/i });
   });
 
   // it("should allow you to make a note", () => {
