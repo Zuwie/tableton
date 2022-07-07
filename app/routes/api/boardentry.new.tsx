a1eba7fa92992fde7a7195aafbb231811f253935
rafaelseifert <rs.frontend@gmail.com>
2022-07-06 20:34:25 -0400

    fix: reading request body

--- app/routes/api/boardentry.new.tsx
+++ app/routes/api/boardentry.new.tsx
@@ -1,5 +1,8 @@
 import type { ActionFunction } from "@remix-run/node";
-import type { createBoardEntry } from "~/models/board.server";
+import { json } from "@remix-run/node";
+import { getUserByDiscordId } from "~/models/user.server";
+import { cors } from "remix-utils";
+import { createBoardEntry } from "~/models/board.server";
 
 type ActionData = {
   boardEntry: Awaited<ReturnType<typeof createBoardEntry>>;
@@ -11,23 +14,20 @@ type ActionData = {
  * @returns A function that returns a promise that resolves to a response object.
  */
 export const action: ActionFunction = async ({ request, params }) => {
-  console.log("request", request);
-  console.log("params", params);
-  // @ts-ignore
-  // const { id, title, content } = request.body;
-  //
-  // const user = await getUserByDiscordId(id);
+  const { id, title, content } = await request.json();
 
-  return "yo";
-  // const boardEntry = await createBoardEntry({
-  //   title,
-  //   body: content,
-  //   gameSystem: "WARHAMMER_40k",
-  //   location: "Bot Town",
-  //   date: new Date(),
-  //   status: 0,
-  //   // @ts-ignore
-  //   userId: user.id,
-  // });
-  // return await cors(request, json<ActionData>({ boardEntry }));
+  const user = await getUserByDiscordId(id);
+
+  if (!user) throw new Error("User with given discord-id doesn't exist.");
+
+  const boardEntry = await createBoardEntry({
+    title,
+    body: content,
+    gameSystem: "WARHAMMER_40k",
+    location: "Bot Town",
+    date: new Date(),
+    status: 0,
+    userId: user.id,
+  });
+  return await cors(request, json<ActionData>({ boardEntry }));
 };
